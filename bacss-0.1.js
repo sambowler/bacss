d6e6a82a9b4c2938816d19e7daf115b8f4ef788d
Sam Bowler <sam@sambowler.com>
2010-09-14 00:39:00 +0100

    Fixed IE bug

--- bacss-0.1.js
+++ bacss-0.1.js
@@ -1,21 +1,24 @@
 // Creates a link element to the latest version of BaCSS
 function createLinkEl() {
-	head =  document.getElementsByTagName('head')[0];
-	cssEl = document.createElement('link');
+	head =  document.getElementsByTagName('head');
+	head = head[0];
+	linkEl = document.createElement('link');
 	linkEl.rel = "stylesheet";
 	linkEl.href = "http://github.com/sambowler/bacss/raw/master/latest.css";
 	linkEl.media = "screen";
 	head.appendChild(linkEl);
 }
 
-// Checks whether the user is using IE < 9
+// Checks whether IE < 9 is in use
 function is_lt_ie9() {
-	if(parseFloat(navigator.userAgent.match(/MSIE (\d+\.\d+);/)[1]) < 9) { return true; } else { return false; };
+	ie_v = parseFloat(navigator.userAgent.match(/MSIE (\d+\.\d+);/));
+	if(ie_v && ie_v[1] < 9) { return true; } else { return false; };
 }
 
-function Bacss(options) {
+function Bacss() {
 	if(is_lt_ie9()) { // If the user is using less than IE9, we need to get the browser to support some features the CSS uses (and support HTML5 elements)
-		head = document.getElementsByTagName('head')[0];
+		head = document.getElementsByTagName('head');
+		head = head[0];
 		ie7js = document.createElement('script');
 		ie7js.src = "http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js";
 		head.appendChild(ie7js);
